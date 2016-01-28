0e54c01b8aa03bf248bc025d546cf20bbcda83ff
jimmyly89 <jimmyly89@gmail.com>
2016-01-29 01:04:51 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -40,7 +40,7 @@ var config = {
         urls: [
 		{
 			symbol: 'calendar-plus-o', 
-			url: 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M'
+			url: 'https://calendar.google.com/calendar/ical/en.australian%23holiday%40group.v.calendar.google.com/public/basic.ics'
 		},
 		{
 			symbol: 'soccer-ball-o',
