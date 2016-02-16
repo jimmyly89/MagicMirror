6c0a4c3c30aa1bf6fc74459134a0d7fc11c6c219
jimmyly89 <jimmyly89@gmail.com>
2016-02-16 21:41:41 +1100

    Update compliments.js

--- js/compliments/compliments.js
+++ js/compliments/compliments.js
@@ -1,31 +1,4 @@
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "compliments.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
-});
 
-
-
-function processData(alltext) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
-        }  
-    } alert(lines);
-}  
 
    
 var compliments = {
