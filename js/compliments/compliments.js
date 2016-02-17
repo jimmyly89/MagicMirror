e9a12e016f906192765ed480264b10e7fdba7bab
jimmyly89 <jimmyly89@gmail.com>
2016-02-18 01:20:33 +1100

    Update compliments.js

--- js/compliments/compliments.js
+++ js/compliments/compliments.js
@@ -45,7 +45,9 @@ var compliments = {
 	complimentList: {
 		'morning': config.compliments.morning,
 		'afternoon': config.compliments.afternoon,
-		'evening': config.compliments.evening
+		'evening': foo(function(test) {
+	console.log(test);
+});
 	},
 	updateInterval: config.compliments.interval || 30000,
 	fadeInterval: config.compliments.fadeInterval || 4000,
