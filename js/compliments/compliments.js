82647a1e7ca1fd1284f4c6cb483e5a098bebedaa
jimmyly89 <jimmyly89@gmail.com>
2016-02-17 21:22:27 +1100

    Update compliments.js

--- js/compliments/compliments.js
+++ js/compliments/compliments.js
@@ -3,85 +3,80 @@ $(document).ready(function() {
         type: "GET",
         url: "compliments.txt",
         dataType: "text",
-        success: function(data) {
-                               
-                               
-                                var lines  = processData(data);
-                                console.log(lines)}
+        success: function(data) {processData(data);}
      });
 });
- 
- 
- 
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-                var lines =[] ;
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
-        } 
-    } return lines
-} 
- 
+
+
    
 var compliments = {
-complimentLocation: '.compliment',
-currentCompliment: '',
-complimentList: {
-'morning': config.compliments.morning,
-'afternoon': config.compliments.afternoon,
-'evening': config.compliments.evening
-},
-updateInterval: config.compliments.interval || 30000,
-fadeInterval: config.compliments.fadeInterval || 4000,
-intervalId: null
+	complimentLocation: '.compliment',
+	currentCompliment: '',
+	complimentList: {
+		'morning': config.compliments.morning,
+		'afternoon': config.compliments.afternoon,
+		'evening': config.compliments.evening
+	},
+	updateInterval: config.compliments.interval || 30000,
+	fadeInterval: config.compliments.fadeInterval || 4000,
+	intervalId: null
 };
+
 /**
-* Changes the compliment visible on the screen
-*/
-compliments.updateCompliment = function (lines) {
-var _list = [];
-var hour = moment().hour();
-// In the followign if statement we use .slice() on the
-// compliments array to make a copy by value.
-// This way the original array of compliments stays in tact.
-if (hour >= 3 && hour < 12) {
-// Morning compliments
-_list = compliments.complimentList['morning'].slice();
-} else if (hour >= 12 && hour < 17) {
-// Afternoon compliments
-_list = compliments.complimentList['afternoon'].slice();
-} else if (hour >= 17 || hour < 3) {
-// Evening compliments
-_list = lines//.slice();
-//compliments.complimentList['evening'].slice();
-} else {
-// Edge case in case something weird happens
-// This will select a compliment from all times of day
-Object.keys(compliments.complimentList).forEach(function (_curr) {
-_list = _list.concat(compliments.complimentList[_curr]).slice();
-});
+ * Changes the compliment visible on the screen
+ */
+compliments.updateCompliment = function () {
+
+
+
+	var _list = [];
+
+	var hour = moment().hour();
+
+	// In the followign if statement we use .slice() on the
+	// compliments array to make a copy by value. 
+	// This way the original array of compliments stays in tact.
+
+	if (hour >= 3 && hour < 12) {
+		// Morning compliments
+		_list = compliments.complimentList['morning'].slice();
+	} else if (hour >= 12 && hour < 17) {
+		// Afternoon compliments
+		_list = compliments.complimentList['afternoon'].slice();
+	} else if (hour >= 17 || hour < 3) {
+		// Evening compliments
+		_list = ['test1', 'test2']//.slice();
+		//compliments.complimentList['evening'].slice();
+	} else {
+		// Edge case in case something weird happens
+		// This will select a compliment from all times of day
+		Object.keys(compliments.complimentList).forEach(function (_curr) {
+			_list = _list.concat(compliments.complimentList[_curr]).slice();
+		});
+	}
+
+	// Search for the location of the current compliment in the list
+	var _spliceIndex = _list.indexOf(compliments.currentCompliment);
+
+	// If it exists, remove it so we don't see it again
+	if (_spliceIndex !== -1) {
+		_list.splice(_spliceIndex, 1);
+	}
+
+	// Randomly select a location
+	var _randomIndex = Math.floor(Math.random() * _list.length);
+	compliments.currentCompliment = _list[_randomIndex];
+
+	$('.compliment').updateWithText(compliments.currentCompliment, compliments.fadeInterval);
+
 }
-// Search for the location of the current compliment in the list
-var _spliceIndex = _list.indexOf(compliments.currentCompliment);
-// If it exists, remove it so we don't see it again
-if (_spliceIndex !== -1) {
-_list.splice(_spliceIndex, 1);
-}
-// Randomly select a location
-var _randomIndex = Math.floor(Math.random() * _list.length);
-compliments.currentCompliment = _list[_randomIndex];
-$('.compliment').updateWithText(compliments.currentCompliment, compliments.fadeInterval);
-}
+
 compliments.init = function () {
-this.updateCompliment(lines);
-this.intervalId = setInterval(function () {
-this.updateCompliment();
-}.bind(this), this.updateInterval)
+
+	this.updateCompliment();
+
+	this.intervalId = setInterval(function () {
+		this.updateCompliment();
+	}.bind(this), this.updateInterval)
+
 }
