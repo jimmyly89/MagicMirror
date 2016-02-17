0bc52d622cf206358efa5a2d4c0956b80ae485cd
jimmyly89 <jimmyly89@gmail.com>
2016-02-18 01:18:08 +1100

    Update compliments.js

--- js/compliments/compliments.js
+++ js/compliments/compliments.js
@@ -76,7 +76,7 @@ console.log(compliments.complimentList['evening'])
 		_list = compliments.complimentList['afternoon'].slice();
 	} else if (hour >= 17 || hour < 3) {
 		// Evening compliments
-		_list = compliments.complimentList['evening'].slice();
+		_list = bar.slice();
 		//compliments.complimentList['evening'].slice();
 	} else {
 		// Edge case in case something weird happens
