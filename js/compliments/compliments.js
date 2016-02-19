5432d3afd046dbc75c4b9ef4bde1e45460027932
jimmyly89 <jimmyly89@gmail.com>
2016-02-19 19:09:23 +1100

    Update compliments.js

--- js/compliments/compliments.js
+++ js/compliments/compliments.js
@@ -45,7 +45,7 @@ var compliments = {
 	complimentList: {
 		'morning': config.compliments.morning,
 		'afternoon': config.compliments.afternoon,
-		'evening': this.bar
+		'evening': foo
 	},
 	updateInterval: config.compliments.interval || 30000,
 	fadeInterval: config.compliments.fadeInterval || 4000,
@@ -76,7 +76,7 @@ console.log(compliments.complimentList['evening'])
 		_list = compliments.complimentList['afternoon'].slice();
 	} else if (hour >= 17 || hour < 3) {
 		// Evening compliments
-		_list = compliments.complimentList['evening'].slice();
+		_list = compliments.complimentList.foo(callback).slice();
 		//compliments.complimentList['evening'].slice();
 	} else {
 		// Edge case in case something weird happens
